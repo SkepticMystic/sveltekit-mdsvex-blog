a27d5cf603c6917f25618102b554a0e66abffb34
Ross Keenan <rossk29@gmail.com>
2023-02-11 10:23:06 +0200

    Fetch & show individual post

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+  const { component, meta } = data;
+</script>
+
+<article class="md">
+  <h1 class="sm:text-5xl text-4xl">{meta.title}</h1>
+  <p>{new Date(meta.createdAt).toDateString()}</p>
+  <div class="flex flex-wrap gap-3 my-3">
+    {#each meta.tags as tag}
+      <span class="bg-base-200 rounded-md px-2 py-1">{tag}</span>
+    {/each}
+  </div>
+
+  <svelte:component this={component} />
+</article>
