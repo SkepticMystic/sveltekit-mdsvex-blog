e12554a3955b0b0be8e1877d0d5a9e916eeab788
Ross Keenan <rossk29@gmail.com>
2023-02-11 10:23:15 +0200

    Bit of styling on the app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="px-[8%] max-w-7xl mx-auto">
+  <nav class="navbar">
+    <ul class="flex gap-4">
+      <li>
+        <a href="/" class="link link-primary link-hover">Home</a>
+      </li>
+      <li>
+        <a href="/blog" class="link link-primary link-hover">Blog</a>
+      </li>
+    </ul>
+  </nav>
+
+  <main>
+    <slot />
+  </main>
+</div>
